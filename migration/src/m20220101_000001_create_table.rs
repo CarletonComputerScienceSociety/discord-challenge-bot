985ac05265440935d92887b6cc5476eda82822aa
Forest Anderson <forestkzanderson@gmail.com>
2022-08-31 14:25:50 -0400

    Set up models

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -3,40 +3,206 @@ use sea_orm_migration::prelude::*;
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
+#[derive(Iden)]
+enum Event {
+    Table,
+    Id,
+    DiscordServerId,
+    DiscordCategoryId,
+}
+
+#[derive(Iden)]
+enum Team {
+    Table,
+    Id,
+    DiscordChannelId,
+    EventId,
+}
+
+#[derive(Iden)]
+enum Participant {
+    Table,
+    Id,
+    DiscordId,
+    TeamId,
+}
+
+#[derive(Iden)]
+enum Submission {
+    Table,
+    Id,
+    SubmissionData,
+    ParticipantId,
+}
+
+const FK_TEAM_EVENT: &str = "fk_team_event";
+const FK_PARTICIPANT_TEAM: &str = "fk_participant_team";
+const FK_SUBMISSION_PARTICIPANT: &str = "fk_submission_participant";
+
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Table for event
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(Event::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Post::Id)
+                        ColumnDef::new(Event::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(ColumnDef::new(Event::DiscordServerId).string().not_null())
+                    .col(ColumnDef::new(Event::DiscordCategoryId).string().not_null())
                     .to_owned(),
             )
-            .await
+            .await?;
+
+        // Table for team
+        manager
+            .create_table(
+                Table::create()
+                    .table(Team::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Team::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Team::DiscordChannelId).string().not_null())
+                    .col(ColumnDef::new(Team::EventId).integer().not_null())
+                    // Create a foreign key to from team to event
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name(FK_TEAM_EVENT)
+                            .from(Team::Table, Team::EventId)
+                            .to(Event::Table, Event::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        // Table for participant
+        manager
+            .create_table(
+                Table::create()
+                    .table(Participant::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Participant::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Participant::DiscordId).string().not_null())
+                    .col(ColumnDef::new(Participant::TeamId).integer().not_null())
+                    // Create a foreign key to from participant to team
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name(FK_PARTICIPANT_TEAM)
+                            .from(Participant::Table, Participant::TeamId)
+                            .to(Team::Table, Team::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        // Table for submission
+        manager
+            .create_table(
+                Table::create()
+                    .table(Submission::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Submission::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(
+                        ColumnDef::new(Submission::SubmissionData)
+                            .string()
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(Submission::ParticipantId).integer().not_null())
+                    // Create a foreign key to from submission to participant
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name(FK_SUBMISSION_PARTICIPANT)
+                            .from(Submission::Table, Submission::ParticipantId)
+                            .to(Participant::Table, Participant::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Drop the foreign key from submission to participant
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
-            .await
-    }
-}
+            .drop_foreign_key(
+                ForeignKey::drop()
+                    .name(FK_SUBMISSION_PARTICIPANT)
+                    .table(Submission::Table)
+                    .to_owned(),
+            )
+            .await?;
 
-/// Learn more at https://docs.rs/sea-query#iden
-#[derive(Iden)]
-enum Post {
-    Table,
-    Id,
-    Title,
-    Text,
+        // Drop the foreign key from team to event
+        manager
+            .drop_foreign_key(
+                ForeignKey::drop()
+                    .name(FK_TEAM_EVENT)
+                    .table(Team::Table)
+                    .to_owned(),
+            )
+            .await?;
+
+        // Drop the foreign key from participant to team
+        manager
+            .drop_foreign_key(
+                ForeignKey::drop()
+                    .name(FK_PARTICIPANT_TEAM)
+                    .table(Participant::Table)
+                    .to_owned(),
+            )
+            .await?;
+
+        // Drop the table for Event
+        manager
+            .drop_table(Table::drop().table(Event::Table).to_owned())
+            .await?;
+
+        // Drop the table for Team
+        manager
+            .drop_table(Table::drop().table(Team::Table).to_owned())
+            .await?;
+
+        // Drop the table for Participant
+        manager
+            .drop_table(Table::drop().table(Participant::Table).to_owned())
+            .await?;
+
+        // Drop the table for Submission
+        manager
+            .drop_table(Table::drop().table(Submission::Table).to_owned())
+            .await?;
+
+        Ok(())
+    }
 }
